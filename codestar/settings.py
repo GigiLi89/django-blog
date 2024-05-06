232afd945cf4aaaa8329ebe9639f32bafcb62c73
GigiLi89 <li.gigi89@gmail.com>
2024-05-06 09:12:18 +0000

    Solved bug issue for database superuser login

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,10 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-cjc#js$z9c+hht%5m4czodeloj&8pr9ioz5dyvt++^(+lo8$82'
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-gigili89-djangoblog-vxdx3jzo4wq.ws-eu110.gitpod.io',
+ALLOWED_HOSTS = ['8000-gigili89-djangoblog-vxdx3jzo4wq.ws-eu111.gitpod.io',
                 '.herokuapp.com']
 
 
@@ -89,6 +91,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://8000-gigili89-djangoblog-vxdx3jzo4wq.ws-eu111.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
