b81bedbb0ef39af63858476e10ee25143b7e3ca5
GigiLi89 <li.gigi89@gmail.com>
2024-05-08 10:53:54 +0000

    walkthrough

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,5 @@ from .models import Post
 # PostList that inherits from the generic.ListView class to display all your posts.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
